b65beabc4aee72f340e05c1c29aa21b465c8fc72
clemkofi <cak.nartey@gmail.com>
2020-12-27 20:10:37 +0100

    Cloud firstore request fixed

--- app.js
+++ app.js
@@ -18,29 +18,6 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("Welcome to Test Vera!"));
 
-// testing the firebase data entry
-app.get("/TestFireBase", async function (req, res) {
-  var firebase_request_ok = 0;
-
-  await firestore
-    .collection(collectionKey)
-    .doc("clem2")
-    .set({'my_self' : "Key_test", 'him_self' : 'Key_Test_2'})
-    .then((res) => {
-      console.log("Data input successful!");
-      firebase_request_ok = 1;
-    })
-    .catch((error) => {
-      console.error("Error writing document: ", error);
-    });
-
-  if (firebase_request_ok == 1){
-    res.send("Request Succesful!");
-  } else {
-    res.send("Request Unsuccesful!");
-  }
-});
-
 // get request to be used by plugin
 app.get("/TestVera", function (req, res) {
   const testData = {
@@ -54,17 +31,89 @@ app.get("/TestVera", function (req, res) {
 
 // post request to use on the vera box
 app.post("/TestVera", async function (req, res) {
-
   console.log("request body : ", req.body);
+
+  console.log("Serial_No: ", req.body.Serial_No);
+  console.log("Unixtimestamp: ", req.body.Unixtimestamp);
 
   var firebase_request_ok = 0;
 
+  let request_data = JSON.parse(JSON.stringify(req.body));
+
+  // reformat the data for the rooms
+  request_data.rooms = req.body.rooms.map((val,i)=>{ 
+    var room_str = "raum";
+    room_str = room_str.concat(i);
+    return {
+      [room_str]: [...val] 
+    }
+  });
+
+  // reformat the data for the temperature sensors
+  request_data.Temperatursensors = req.body.Temperatursensors.map((val,i)=>{ 
+    var temp_str = "tempsensor";
+    temp_str = temp_str.concat(i);
+    return {
+      [temp_str]: [...val] 
+    }
+  });
+
+  // reformat the data for the DWS sensors
+  request_data.DoorWindowsensors = req.body.DoorWindowsensors.map((val,i)=>{ 
+    var DWS_str = "DWsensor";
+    DWS_str = DWS_str.concat(i);
+    return {
+      [DWS_str]: [...val] 
+    }
+  });
+
+  // reformat the data for the Motion sensors
+  request_data.Motionsensors = req.body.Motionsensors.map((val,i)=>{ 
+    var Motion_str = "MotionSensor";
+    Motion_str = Motion_str.concat(i);
+    return {
+      [Motion_str]: [...val] 
+    }
+  });
+
+  // reformat the data for the Humidity sensors
+  request_data.Humiditisensors = req.body.Humiditisensors.map((val,i)=>{ 
+    var humidity_str = "humiditySensor";
+    humidity_str = humidity_str.concat(i);
+    return {
+      [humidity_str]: [...val] 
+    }
+  });
+
+  // reformat the data for the Switches sensors
+  request_data.Switches = req.body.Switches.map((val,i)=>{ 
+    var switches_str = "switch";
+    switches_str = switches_str.concat(i);
+    return {
+      [switches_str]: [...val] 
+    }
+  });
+
+  // reformat the data for the light sensors
+  request_data.Lichtsensors = req.body.Lichtsensors.map((val,i)=>{ 
+    var light_str = "lightSensor";
+    light_str = light_str.concat(i);
+    return {
+      [light_str]: [...val] 
+    }
+  });
+
+  // console.log(request_data);
+
+  // console.log(req.body);
+
+  // cloud firestore request
   await firestore
     .collection(collectionKey)
     .doc(req.body.Serial_No)
-    .collection(req.body.Unixtimestamp)
-    .doc(req.body.Unixtimestamp)
-    .set(req.body)
+    .collection(req.body.Unixtimestamp.toString())
+    .doc(req.body.Unixtimestamp.toString())
+    .set(request_data)
     .then((res) => {
       console.log("Data input successful!");
       firebase_request_ok = 1;
@@ -73,11 +122,13 @@ app.post("/TestVera", async function (req, res) {
       console.error("Error writing document: ", error);
     });
 
-  if (firebase_request_ok == 1)   {
+  if (firebase_request_ok == 1) {
     res.send("Request Succesful!");
   } else {
     res.send("Request Unsuccesful!");
   }
+
+  // res.send("Ok to go!");
 });
 
 // port for the server to listen on 3000 or port from heroku
